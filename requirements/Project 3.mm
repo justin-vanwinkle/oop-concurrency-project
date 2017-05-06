4be5ea2755b37123ee1a60849e50ae4c670f0357
Justin VanWinkle <vanwinkle.justin@gmail.com>
2017-05-06 07:58:49 -0400

    prep for p4

--- requirements/Project 3.mm
+++ requirements/Project 3.mm
@@ -2,13 +2,30 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1492464895265" ID="ID_1263038813" MODIFIED="1492464904695" TEXT="Project 3">
 <node CREATED="1492464907132" ID="ID_740475676" MODIFIED="1492464950050" POSITION="right" TEXT="Implement thread for each Job">
-<node CREATED="1492465004509" ID="ID_892784601" MODIFIED="1492465036221" TEXT="Jobs only run on a Ship in Dock"/>
-<node CREATED="1492465037277" ID="ID_1506605518" MODIFIED="1492465085961" TEXT="When all Jobs complete, Ship leaves Dock"/>
-<node CREATED="1492465185883" ID="ID_1166459526" MODIFIED="1492465212771" TEXT="Once Docked, all Jobs should start"/>
+<node CREATED="1492465004509" ID="ID_892784601" MODIFIED="1492873518876" TEXT="Jobs only run on a Ship in Dock">
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1492465286325" ID="ID_6281846" MODIFIED="1492465330155" POSITION="right" TEXT="Job threads should be &quot;started&quot; as the Job is read in from file"/>
-<node CREATED="1492465332255" ID="ID_1534969157" MODIFIED="1492465386301" POSITION="right" TEXT="Use delays to simulate progress of each Job"/>
-<node CREATED="1492465387160" ID="ID_1897577722" MODIFIED="1492465403874" POSITION="right" TEXT="Use JProgressBar to show progress"/>
-<node CREATED="1492465405366" ID="ID_433922476" MODIFIED="1492465450584" POSITION="right" TEXT="Use JButtons to suspend or cancel a Job"/>
+<node CREATED="1492465037277" ID="ID_1506605518" MODIFIED="1492916110729" TEXT="When all Jobs complete, Ship leaves Dock">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1492465185883" ID="ID_1166459526" MODIFIED="1492873539185" TEXT="Once Docked, all Jobs should start">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1492465286325" ID="ID_6281846" MODIFIED="1492872749210" POSITION="right" TEXT="Job threads should be &quot;started&quot; as the Job is read in from file">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1492465332255" ID="ID_1534969157" MODIFIED="1492872758319" POSITION="right" TEXT="Use delays to simulate progress of each Job">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1492465387160" ID="ID_1897577722" MODIFIED="1492914380339" POSITION="right" TEXT="Use JProgressBar to show progress">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1492465405366" ID="ID_433922476" MODIFIED="1492977949635" POSITION="right" TEXT="Use JButtons to suspend or cancel a Job">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1492893491483" ID="ID_82860508" MODIFIED="1492914359816" POSITION="left" TEXT="JTree docked ship should show Jobs">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 </map>
